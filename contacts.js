01b0d15a97bea82bb6c3c5f47b7d9fb196960231
omykhalska <omykhalska@gmail.com>
2022-05-18 23:24:36 +0300

    Remove commented out code

--- contacts.js
+++ contacts.js
@@ -2,7 +2,6 @@ const fs = require('fs').promises;
 const path = require('path');
 const { nanoid } = require('nanoid');
 
-// const contactsPath = path.resolve('db', 'contacts.json');
 const contactsPath = path.join(__dirname, 'db', 'contacts.json');
 
 const listContacts = async () => {
